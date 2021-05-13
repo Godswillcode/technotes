8ef83c8159dff24d3e79fbbc0cf84f0d0fc1374e
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2021-05-13 14:11:26 +0100

    added view

--- appsupport.js
+++ appsupport.js
@@ -56,6 +56,6 @@ export function basicErrorHandler(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ?
     err : {}
-    res.status(err.status || 5000);
+    res.status(err.status || 500);
     res.render('error')
 }
